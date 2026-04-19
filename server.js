750cdd29688f94984b6bf00a2f6ba8a99050a516
tankarsana-byte <tankarsana@gmail.com>
2026-04-19 18:48:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,21 @@ app.use(express.json());
 let latestData = {};
 
 // Test route
-app.get("/", (req, res) => {
-  res.send("Server Running ✅");
-});
+app.get("/data", (req, res) => {
+  const now = Date.now();
 
+  // अगर 5 सेकंड से data नहीं आया → ESP32 OFF
+  if (!lastUpdated || (now - lastUpdated > 5000)) {
+    return res.json({
+      status: "offline"
+    });
+  }
+
+  res.json({
+    status: "online",
+    ...latestData
+  });
+});
 // ESP32 data receive karega
 app.post("/data", (req, res) => {
   latestData = req.body;
